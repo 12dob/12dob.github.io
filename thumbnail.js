8454d61a11c813e58d0a5a388b4c6f2a421601d5
12dob <60208656+12dob@users.noreply.github.com>
2022-03-16 11:24:03 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    let targetImage = document.querySelector("#smart-image");
-
-   
-   if (targetImage.classList.contains("small")) {
-      targetImage.addEventListener("click", function() {
+  targetImage.addEventListener("click", function() {
     alert("点击了图片");
       targetImage.classList.remove("small");
 });
+   
+   if (targetImage.classList.contains("small")) {
+    
 } else {
     targetImage.addEventListener("click", function() {
-    alert("点击了图片");
       targetImage.classList.add("small");
 });
    
