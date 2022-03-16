adc025d0b747a65a703f6ef270496faa49d40f86
12dob <60208656+12dob@users.noreply.github.com>
2022-03-16 11:17:55 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   alert("Hello JavaScript");
    let targetImage = document.querySelector("#smart-image");
    targetImage.addEventListener("click", function() {
     alert("点击了图片");
       targetImage.classList.remove("small");
 });
    
-   
+   if (targetImage.classList.contains("small")) {
+    // ...
+} else {
+   targetImage.classList.add("small");
+}
    
    
    
